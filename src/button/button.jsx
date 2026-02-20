5d0cd6d73395781840fedefebe60865573f61121
Caleb Hessing <cckkhh7@gmail.com>
2026-02-20 02:12:53 -0700

    ported button.html to button.jsx right but now login page is wonky

--- src/button/button.jsx
+++ src/button/button.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './button.css';
 
 export function Button() {
   return (
     <main>
-      <div>buton displayed here</div>
+      <div>
+        <button id="the-button">DON'T PRESS ME</button>
+      </div>
+      <table>
+        <tr><th>Notifications</th></tr>
+        <tr><td>JohnDoe42 pressed the button.</td></tr>
+        <tr><td>JoeMama000 pressed the button.</td></tr>
+        <tr><td>SantaClause25 pressed the button.</td></tr>
+        <tr><td>xXgamerXx pressed the button.</td></tr>
+        <tr><td>skibidiGyatt6767 pressed the button.</td></tr>
+      </table>
     </main>
   );
 }
