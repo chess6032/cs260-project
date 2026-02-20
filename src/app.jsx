7d28c4f60dacf4b0fc188bec2f5cc47f4edc9aaa
Caleb Hessing <cckkhh7@gmail.com>
2026-02-20 01:39:36 -0700

    added App component

---
+++ src/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; // for CSS Bootstrap
+import './app.css';
+
+export default function App() {
+    return <div className="pagebody bg-dark text-light">App will display here (trust).</div>;
+}
