8db022f8b35a18421d9ec2ad02cc45021aaf4c62
Caleb Hessing <cckkhh7@gmail.com>
2026-02-20 01:25:07 -0700

    rewrote index.html and added index.jsx

---
+++ index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './src/app'; // TODO: make ts
+
+const root = ReactDOM.createRoot(document.getElementById('root')); // hook to id='root' element
+root.render(<App />); // fill hooked element w/ App component (./src/app.jsx)
