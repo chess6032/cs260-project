e53e6d99f61926f1848fe9d64c48480bce3d10a6
Caleb Hessing <cckkhh7@gmail.com>
2026-02-20 01:52:20 -0700

    wrapped App component in BrowserRouter and changed <a> to <NavLink>

--- src/app.jsx
+++ src/app.jsx
@@ -2,23 +2,34 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; // for CSS Bootstrap
 import './app.css';
 
+// ROUTER STUFF
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Scores } from './scores/scores';
+import { About } from './about/about';
+
 export default function App() {
-    return <div className="pagebody bg-dark text-light"> {/*Serves same purpose as <body> did before*/}
-    <header>
-      {/* TODO: This nav menu is a placeholder...I don't anticipate I'll have it in the end product... */}
-      <nav className="ckh-nav-bar">
-        <span id="brand">Don't press the button</span>
-        <div className="ckh-vert-separator"></div>
-        <a className="ckh-btn" href="login.html">Home</a>
-        <a className="ckh-btn" href="button.html">Button</a>
-        <a className="ckh-btn" href="banned.html">After pressing the button</a>
-      </nav>
-    </header>
-    <main>Main content here.</main> {/* There MUST be a <main> tag or else <footer> won't be fixed to the bottom of the page. */}
-    <footer>
-      <span id="author-name">Caleb Hessing</span>
-      {/* <div class="ckh-vert-separator"></div> */}
-      <a id="repo-link" href="https://github.com/chess6032/cs260-project">GitHub</a>
-    </footer>
-    </div>;
+    return (
+      <BrowserRouter>
+        <div className="pagebody bg-dark text-light"> {/*Serves same purpose as <body> did before*/}
+          <header>
+            {/* TODO: This nav menu is a placeholder...I don't anticipate I'll have it in the end product... */}
+            <nav className="ckh-nav-bar">
+              <span id="brand">Don't press the button</span>
+              <div className="ckh-vert-separator"></div>
+              <NavLink className="ckh-btn" to="">Home</NavLink> {/* go to log-in page by default */}
+              <NavLink className="ckh-btn" to="button">Button</NavLink>
+              <NavLink className="ckh-btn" to="banned">After pressing the button</NavLink>
+            </nav>
+          </header>
+          <main>Main content here.</main> {/* There MUST be a <main> tag or else <footer> won't be fixed to the bottom of the page. */}
+          <footer>
+            <span id="author-name">Caleb Hessing</span>
+            {/* <div class="ckh-vert-separator"></div> */}
+            <a id="repo-link" href="https://github.com/chess6032/cs260-project">GitHub</a>
+          </footer>
+        </div>
+      </BrowserRouter>
+    );
 }
