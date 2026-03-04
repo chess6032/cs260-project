768589ed2b4159482d240460efbf5cd9d36ff551
Caleb Hessing <cckkhh7@gmail.com>
2026-03-03 22:06:01 -0700

    updated banned.jsx with placeholder js for 3rd party service calling

--- src/banned/banned.jsx
+++ src/banned/banned.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; // I don't think this page uses bootstrap but just in case...
 import './banned.css';
 
+// react pt. 2: added placeholder JS for 3rd-party service calls. (See Simon React Pt. 2 --> About component)
+
 export function Banned() {
+  // PLACEHOLDER FOR 3RD-PARTY SERVICE CALLS
+  const [quote, setQuote] = React.useState("Loading something wise...");
+
+  React.useEffect(() => {
+    setQuote(`"You can believe any quote you read on the internet—especially if it comes from a cool external API call."`)
+  }, []); // by passing in an empty subarray for the dependencies, this useEffect callback will only be called the first time the Banned component renders.
+
   return (
     <main className="banned-main">
       <h1>YOU PRESSED THE BUTTON!!</h1>
       <p>You have proven that you are NOT a responsible agent of freewill. As such, your freewill has been restricted: you will never have the 
         choice of pressing the button ever again.</p>
       <div className="quote-container">
-        <span className="quote">"You can believe any quote you read on the internet&mdash;especially if it comes from a cool external API call." <br/> <span class="quote-attribution">Kanye West</span></span>
+        <span className="quote">{quote}<br/><span class="quote-attribution">Kanye West</span></span>  
         <img id="image" src="Kanye_West_at_the_2009_Tribeca_Film_Festival_(crop_2)-DavidShankbone.jpg" height="200" />
       </div>
     </main>
