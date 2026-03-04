a677b0ea7c7b49e8f6d11798e3915f17e2170189
Caleb Hessing <cckkhh7@gmail.com>
2026-03-04 00:27:30 -0700

    made websocket placeholder better

--- src/button/notifs.jsx
+++ src/button/notifs.jsx
@@ -3,34 +3,49 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './notifs.css';
 
 export function NotifTable() {
-  const [notifList, setNotifList] = React.useState([]);
+  const MAX_NOTIF_LIST_LEN = 10
+
+  // TODO: find a better way to implement the notification list 
+  // than a table whose rows are filled by an always-ten-element array
+  const [notifList, setNotifList] = React.useState(Array(MAX_NOTIF_LIST_LEN-1).fill(null).concat("WEBSOCKET PLACEHOLDER"));
+
 
   const addNotif = (user="another user") => {
-    setNotifList((prevNotifs) => [...prevNotifs, { name: user, num: prevNotifs.length }]);
-                                                      // TODO: remove num member.
-                                                      // (right now I use it b/c table rows HAVE to have a unique "key" property.)
+    setNotifList((prevNotifs) => {
+      //  .concat({ name: user, num: prevNotifs.length });
+      if (prevNotifs.length > MAX_NOTIF_LIST_LEN-1) {
+        return prevNotifs.slice(1, MAX_NOTIF_LIST_LEN).concat({ name: user});
+      } else {
+        return prevNotifs.concat({ name: user});;
+      }
+    });
+    
   };
 
+
+
   // placeholder for websocket
+  const randomName = () =>
+    ['JohnDoe42', 'JoeMama000', 'SantaClause25', 'xXgamerXx', 'skibidiGyatt6767'].at(Math.floor(Math.random() * 5)); 
+  
   React.useEffect(() => {
     console.log('rendered');
     const interval = setInterval(() => {
-      addNotif(`user${notifList.length}`);
+      addNotif(randomName());
     }, 1000);
 
     return () => clearInterval(interval); // cleanup
-  }, [notifList]); // TODO: remove notifList for the dependency list. 
-                   // (it was the easiest way to get the "user1", "user2", "user3" thing set up,
-                   // but it's re-rendering the page every time notifList is updated, which is 
-                   // poor performance and won't be necessary when the usernames will be 
-                   // coming from websocket itself.)
+  }, []); 
 
   return (
     <table>
       <thead><tr><th>Notifications</th></tr></thead>
       <tbody>
-        {notifList.map((fool) => (
-          <tr key={fool.num}><td>{fool.name} pressed the button.</td></tr>
+        {notifList.map((fool) => ( fool ? (
+          fool.name ? <tr><td>{fool.name} pressed the button.</td></tr>
+          : <tr><td>{fool}</td></tr>
+          ) 
+          : <tr><td>&nbsp;</td></tr>
         ))}
       </tbody>
     </table>
