616de34dbbc82fff1a78bda136ec24a998531fab
Caleb Hessing <cckkhh7@gmail.com>
2026-03-03 22:50:53 -0700

    added navigation from login to button

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,27 +1,35 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; // I don't think this page uses bootstrap but just in case...
 import './login.css';
+import { useNavigate } from 'react-router-dom';
 
 export function Login() {
+  const navigate = useNavigate();
+
+  function loginUser() {
+    console.log("logged in user");
+    navigate("/button");
+  }
+
   return (
     <main className="login-main">
       <h1>Come on in.</h1> 
       <h2>But please&mdash;don't press the button.</h2>
-      <form id="login-form">
+      <div id="login-form">
 
         <div id="login-input">
           <span>Email:</span>
           <input type="text" placeholder="email@email.com"/>
-  
+
           <span>Password:&nbsp;</span>
           <input type="password" placeholder="password"/>
         </div>
 
         <div id="login-buttons">
-          <button className="ckh-btn">Login</button>
-          <button className="ckh-btn">Register</button>
+          <button className="ckh-btn" onClick={loginUser}>Login</button>
+          <button className="ckh-btn" onClick={loginUser}>Register</button>
         </div>
-      </form>
+      </div>
     </main>
   );
 }
