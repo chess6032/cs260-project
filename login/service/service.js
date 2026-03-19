3b4ba9c00df8bb26738de19d5daa391d0d0b5d3c
Caleb Hessing <cckkhh7@gmail.com>
2026-03-18 20:17:30 -0600

    startup service - created endpoint stubs

---
+++ login/service/service.js
@@ -0,0 +1,30 @@
+// service.js
+// these are stubs for the service endponts that we'll fill in later.
+
+const express = require('express');
+const app = express();
+
+// registration
+app.post('/api/auth', async (req, res) => {
+  res.send({ email: 'marta@id.com' });
+});
+
+// login
+app.put('/api/auth', async (req, res) => {
+  res.send({ email: 'marta@id.com' });
+});
+
+// logout
+app.delete('/api/auth', async (req, res) => {
+  res.send({});
+});
+
+// getMe
+app.get('/api/user', async (req, res) => {
+  res.send({ email: 'marta@id.com' });
+});
+
+const port = 3000;
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`);
+});
