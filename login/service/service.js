92dfe0c7e2a2f1ce85c0ff6d3912e693fcc72909
Caleb Hessing <cckkhh7@gmail.com>
2026-03-18 20:35:56 -0600

    simple registration endpoint

--- login/service/service.js
+++ login/service/service.js
@@ -4,12 +4,47 @@
 const express = require('express');
 const app = express();
 
+// ~~~~~~~~~~~~~~~ HELPER FUNCTIONS & STUFF ~~~~~~~~~~~~~~~
+
+const bcrypt = require('bcryptjs');
+
+const users = [];
+
+async function createUser(email, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+  };
+
+  users.push(user);
+
+  return user;
+}
+
+function getUser(field, value) {
+  if (value) {
+    return users.find((user) => user[field] === value);
+  }
+  return null;
+}
+
+// ~~~~~~~~~~~~~~~ ENDPOINTS ~~~~~~~~~~~~~~~
+
 // middleware: parse request's JSON body
 app.use(express.json());
 
 // registration
 app.post('/api/auth', async (req, res) => {
-  res.send(req.body);
+  if (await getUser('email', req.body.email)) { // check if user exists
+    // FAIL: email already exists
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    // SUCCESS: new user
+    const user = await createUser(req.body.email, req.body.password);
+    res.send({ email: user.email });
+  }
 });
 
 // login
