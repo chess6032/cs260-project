baa43326465def31fc960f99f46dc80137a39579
Caleb Hessing <cckkhh7@gmail.com>
2026-03-30 03:44:26 -0600

    copied Simon's testConnection()

---
+++ service/database.js
@@ -0,0 +1,20 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.username}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('startup');
+const users = db.collection('users');
+
+// This will asynchronously test the connection
+// and exit the process if it fails.
+(async function testConnection() {
+    try {
+        await db.command({ ping: 1 });
+        console.log(`Connected to ${config.hostname}`);
+    } catch (e) {
+        console.log(`Failed to connect to ${config.hostname}: ${e.message}`);
+        process.exit(1);
+    }
+})();
+
